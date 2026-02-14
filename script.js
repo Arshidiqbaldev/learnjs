cda116399fd8b96ffd20fc00ad7d0258dcc17a23
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-02-14 11:02:38 +0500

    scroll behave

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
-
-
 let heroDesc = document.createElement("p");
 
 heroDesc.textContent = `Excepturi debitis saepe quia ex libero sunt provident 
 maiores harum commodi eius natusdoloremqu`;
 
+let myDiv = document.getElementById("content");
 
+myDiv.insertAdjacentElement("afterbegin", heroDesc);
 
-let myDiv = document.getElementById('content');
-
-myDiv.insertAdjacentElement('afterbegin', heroDesc);
-
-
-
-document.querySelector(".descript").classList.add('tagline');
-
+document.querySelector(".descript").classList.add("tagline");
 
 document.getElementById("para").classList.add("desc");
 
-document.querySelector("#cta").classList.add('btn');
-
+document.querySelector("#cta").classList.add("btn");
 
 let newDesc = document.createElement("h2");
 
-
 newDesc.textContent = "hi this is the newDesc";
-newDesc.classList.add('tagline');
 
-
-
+newDesc.classList.add("tagline");
 
 let myDiv2 = document.querySelector("#child");
 
-
-myDiv2.insertAdjacentElement('afterbegin', newDesc)
-
-
-
-
-
-
+myDiv2.insertAdjacentElement("afterbegin", newDesc);
