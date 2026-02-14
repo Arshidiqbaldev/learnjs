2a7eaa1dfa2d5f3062864533dc403a8dcd6e164e
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-02-14 10:39:59 +0500

    the js added

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
 
+let heroDesc = document.createElement("p");
+
+heroDesc.textContent = `Excepturi debitis saepe quia ex libero sunt provident 
+maiores harum commodi eius natusdoloremqu`;
 
 
-{
-    let p = document.getElementById("demo");
 
-b1.onclick = function () {
-  p.setAttribute("style","color:blue; font-size:18px;");
-};
+let myDiv = document.getElementById('content');
 
-b2.onclick = function () {
-  p.style.cssText = "color:yellow ; font-size:30px;";
-};
+myDiv.insertAdjacentElement('afterbegin', heroDesc);
 
-b3.onclick = function () {
-  p.classList.toggle("red");
-}
-}
+
+
+document.querySelector(".descript").classList.add('tagline');
+
+
+document.getElementById("para").classList.add("desc");
+
+document.querySelector("#cta").classList.add('btn');
+
+
+let newDesc = document.createElement("h2");
+
+
+newDesc.textContent = "hi this is the newDesc";
+newDesc.classList.add('tagline');
+
+
+
+
+let myDiv2 = document.querySelector("#child");
+
+
+myDiv2.insertAdjacentElement('afterbegin', newDesc)
+
+
+
+
+
+
